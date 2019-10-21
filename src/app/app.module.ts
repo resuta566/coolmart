4be63a3c5aa4fe41e6d984ee9b016b740a92d7e7
James Lester Orfano <resuta566@gmail.com>
2019-10-21 18:14:46 +0800

    Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { HomenavbarComponent } from './home/homenavbar/homenavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CartComponent,
+    HomenavbarComponent
   ],
   imports: [
     BrowserModule,
