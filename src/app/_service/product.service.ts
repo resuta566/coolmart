9a016df1bf36206f6fcc920e8d0297645094ed99
James Lester Orfano <resuta566@gmail.com>
2019-11-08 09:20:07 +0800

    Shop & Filter

--- src/app/_service/product.service.ts
+++ src/app/_service/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, of} from 'rxjs';
 import { tap, catchError, map } from "rxjs/operators";
 
@@ -10,7 +10,7 @@ import { Products } from '@app/_models/products';
   providedIn: 'root'
 })
 export class ProductService {
-  null = '';
+  null = null;
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type' : 'application/json'})
   };
@@ -31,24 +31,72 @@ export class ProductService {
     }
   }
 
-  getProducts(keyword?, brand?, category?) {
-    let actualKeyword = keyword ? keyword : this.null;
-    let actualbrandId = brand ? brand : this.null;
-    let actualCategoryId = category ? category : this.null;
-    return this.http.get<Products[]>(`${environment.apiUrl}/api/items`,
-      { params:
-        {
-          name: actualKeyword,
-          brand_id: actualbrandId,
-          category_id: actualCategoryId
+  getProducts(keyword?, brand?, category?, type?, min?, max?) {
+    let actualKeyword = keyword ? keyword : '';
+    let actualbrand = brand ? brand : this.null;
+    let actualCategory = category ? category : this.null;
+    let actualType = type ? type : this.null;
+    let actualMin = min ? min : '';
+    let actualMax = max ? max : '';
+    //The HttpParams
+    let prodparams = new HttpParams().set('name', actualKeyword).set('min',actualMin).set('max', actualMax);
+    if(actualbrand){
+      if(actualbrand.length !== 0){
+        //If actualBrandArray is not 0 loop else delete
+        for(let bid of actualbrand){
+         prodparams = prodparams.set(`brand[${bid}]`, bid);
         }
-      }).pipe(
+      }else {
+        for(let bid of actualbrand){
+          prodparams = prodparams.delete(`brand[${bid}]`);
+         }
+      }
+    }else if(actualbrand == null){
+      //Delete the params if there is no array
+        prodparams = prodparams.delete(`brand[]`);
+    }
+
+    if(actualCategory){
+      if(actualCategory.length !== 0){
+        //If actualCategoryArray is not 0 loop else delete
+        for(let cid of actualCategory){
+          prodparams = prodparams.set(`category[${cid}]`, cid);
+         }
+      }else{
+        for(let cid of actualCategory){
+          prodparams = prodparams.delete(`category[${cid}]`);
+         }
+      }
+    }else if(actualCategory == null){
+      //Delete the params if there is no array
+        prodparams = prodparams.delete(`category[]`);
+    }
+
+    if(actualType){
+      if(actualType.length !== 0){
+        //If actualCategoryArray is not 0 loop else delete
+        for(let tid of actualType){
+          prodparams = prodparams.set(`type[${tid}]`, tid);
+         }
+      }else{
+        for(let tid of actualType){
+          prodparams = prodparams.delete(`type[${tid}]`);
+         }
+      }
+    }else if(actualType == null){
+      //Delete the params if there is no array
+          prodparams = prodparams.delete(`type[]`);
+
+    }
+    return this.http.get<Products[]>(`${environment.apiUrl}/api/items`,
+      { params: prodparams }).pipe(
           tap(_ => console.log('fetched products')),
           catchError(this.handleError<Products[]>('getProducts', []))
-          );
+        );
   }
 
   getProduct(slug: string): Observable<Products> {
+    //Get Single Product
     return this.http.get<Products>(`${environment.apiUrl}/api/items/${slug}`).pipe(
       tap(_ => console.log('fetched product'))
       );
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy, ViewChildren, QueryList, ElementRef } from '@angular/core';
 import { ProductService } from '@app/_service/product.service';
 import { environment } from '@environments/environment';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { BrandService } from '@app/_service/brand.service';
 import { DOCUMENT } from '@angular/common';
 import { CategoriesService } from '@app/_service/categories.service';
@@ -16,10 +16,15 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit, OnDestroy {
+
+  @ViewChildren("brandscbox") brandscbox: QueryList<ElementRef>;
+  @ViewChildren("categorycbox") categorycbox: QueryList<ElementRef>;
+  @ViewChildren("typecbox") typecbox: QueryList<ElementRef>;
   private destroy$: Subject<boolean> = new Subject<boolean>();
   btnclass="button add_to_cart_button addToCartBtn";
   label = "Add to cart";
   products: Object;
+  page: any;
   imgThumb: string;
   img: string;
   apiImgUrl = `${environment.apiUrl}`;
@@ -31,10 +36,16 @@ export class ShopComponent implements OnInit, OnDestroy {
   limitType = 4;
   keyword: string;
   optKeyword = "";
+  min = 0;
+  max = 0;
+  mini: number;
+  maxi: number;
+  brandArray: Array<any> = [];
+  categoryArray: Array<any> = [];
+  typeArray: Array<any> = [];
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private productService: ProductService,
-    private router: Router,
     private brandService: BrandService,
     private categoriesService: CategoriesService,
     private typeService: TypesService,
@@ -47,7 +58,7 @@ export class ShopComponent implements OnInit, OnDestroy {
       this.keyword = rt.get('search');
       this.getProducts(this.keyword);
       if(this.keyword == null){
-        this.titleService.setTitle(  `Buy at Best Pirce | Cool Mart` );
+        this.titleService.setTitle(  `Buy at Best Price | Cool Mart` );
       }else{
       this.titleService.setTitle(  `${this.keyword} - Buy ${this.keyword} at Best Pirce | Cool Mart` );
       }
@@ -64,57 +75,128 @@ export class ShopComponent implements OnInit, OnDestroy {
   }
 
 
-  getProducts(keyword?) {
-    let actualKeyword = keyword ? keyword : this.optKeyword;
-    this.productService.getProducts(actualKeyword).pipe(takeUntil(this.destroy$)).subscribe((datas: any) => {
-      this.products = datas.data;
-      },
-        error => {
-        console.log(error);
-    });
+  getProducts(keyword?, brand?, category?, type?, min?, max?) {
+    //Calling the getProducts service from the api
+    this.productService.getProducts(keyword, brand, category, type, min, max)
+          .pipe(takeUntil(this.destroy$)).subscribe((datas: any) => {
+                this.products = datas.data;
+                this.page = datas.meta;
+                },
+                  error => {
+                  console.log(error);
+              });
+  }
+
+  filter(){
+    //this will filter what the user check
+    this.getProducts(this.keyword, this.brandArray, this.categoryArray, this.typeArray, this.mini, this.maxi);
   }
 
   getBrands() {
     this.brandService.getBrands().pipe(takeUntil(this.destroy$)).subscribe((brands: any)=>{
       this.brands = brands.data;
-    })
+    });
   }
 
   getCategories() {
     this.categoriesService.getCategories().pipe(takeUntil(this.destroy$)).subscribe((categories: any)=>{
       this.categories = categories.data;
-    })
+    });
   }
 
   getTypes() {
     this.typeService.getTypes().pipe(takeUntil(this.destroy$)).subscribe((types: any)=>{
       this.types = types.data;
-    })
-  }
-
-  addToCart(){
-    this.router.navigate(['/cart']);
+    });
   }
 
   showMore(limit: number){
-    // console.log(limit);
     this.limit = limit;
   }
   showLess(){
     this.limit = 4;
   }
   showMoreCat(limit: number){
-    // console.log(limit);
     this.limitCat = limit;
   }
   showLessCat(){
     this.limitCat = 4;
   }
   showMoreType(limit: number){
-    // console.log(limit);
     this.limitType = limit;
   }
   showLessType(){
     this.limitType = 4;
+  }
+
+  getBrandId(id: any, isChecked: boolean){
+    if(isChecked){
+      this.brandArray.push(id);
+      this.filter();
+    }else {
+      let index = this.brandArray.indexOf(id);
+      this.brandArray.splice(index,1);
+      this.filter();
+    }
+  }
+  getCategoryId(id: number, isChecked: boolean){
+    if(isChecked){
+      this.categoryArray.push(id);
+      this.filter();
+    }else{
+      let index = this.categoryArray.indexOf(id);
+      this.categoryArray.splice(index,1);
+      this.filter();
+    }
+  }
+
+  getTypeId(id: number, isChecked: boolean){
+    if(isChecked){
+      this.typeArray.push(id);
+      this.filter();
+    }else{
+      let index = this.typeArray.indexOf(id);
+      this.typeArray.splice(index, 1);
+      this.filter();
+    }
+  }
+
+  reset(option: string){
+    if(option == 'all'){
+      if(this.keyword == null){
+        window.location.reload();
+      }else{
+        window.location.reload();
+        this.getProducts(this.keyword);
+      }
+    }else if(option == 'brands'){
+      this.brandscbox.forEach((element) => {
+        element.nativeElement.checked = false;
+      });
+      this.brandArray.splice(0,this.brandArray.length);
+      this.filter();
+    }else if(option == 'categories'){
+      this.categorycbox.forEach((element) => {
+        element.nativeElement.checked = false;
+      });
+      this.categoryArray.splice(0,this.categoryArray.length);
+      this.filter();
+    }else if(option == 'types'){
+      this.typecbox.forEach((element) => {
+        element.nativeElement.checked = false;
+      });
+      this.typeArray.splice(0,this.typeArray.length);
+      this.filter();
+    }
+  }
+
+  pricerange(){
+    this.filter();
+  }
+
+  priceClear(){
+    this.mini = 0;
+    this.maxi = 0;
+    this.pricerange();
   }
 }
