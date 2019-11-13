7485313288503e6dc553e58a20040643fe1e2843
James Lester Orfano <resuta566@gmail.com>
2019-11-13 13:57:39 +0800

    Notif on Not Auth User

--- src/app/_helpers/core/auth.guard.ts
+++ src/app/_helpers/core/auth.guard.ts
@@ -1,11 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthenticationService } from '@app/_service';
 
+import { NOTYF } from '@app/_helpers/notyf.token';
+import { Notyf } from 'notyf';
+
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
     constructor(
+        @Inject(NOTYF) private notyf: Notyf,
         private router: Router,
         private authenticationService: AuthenticationService
     ) { }
@@ -16,9 +20,9 @@ export class AuthGuard implements CanActivate {
             // logged in so return true
             return true;
         }else{
-          alert('Please Log In');
           // not logged in so redirect to login page with the return url
           this.router.navigate(['/sign_in'], { queryParams: { returnUrl: state.url } });
+          this.notyf.error('Please Log In!');
           return false;
         }
     }
