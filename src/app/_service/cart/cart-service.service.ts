10eabca0a1229e21e5ab867a891fa6de36055e19
James Lester Orfano <resuta566@gmail.com>
2020-01-17 17:23:55 +0800

    html update

--- src/app/_service/cart/cart-service.service.ts
+++ src/app/_service/cart/cart-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, empty} from 'rxjs';
+import { Observable, empty, of} from 'rxjs';
 import { tap, catchError, map } from "rxjs/operators";
 
 import { environment } from '@environments/environment';
